14bcb2f703153dccaf348a0eee936975d9f7b0a7
myvoltron <leejs8266@gmail.com>
2022-03-19 23:11:57 +0900

    REST API, GET까지 구현

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express'); 
 const app = express(); 
 
-app.set('port', 8080); 
+// 임시 데이터
+const posts = [
+    { id: 1, title: 'title1', content: 'content1'},
+    { id: 2, title: 'title2', content: 'content2'},
+    { id: 3, title: 'title3', content: 'content3'},
+]; 
+
+app.set('port', 8083); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
+
+app.get('/', (req, res) => {
+    res.send('hi plz use api'); 
+}); 
+
+// 글 목록을 제공하는 라우팅
+app.get('/api/post', (req, res) => {
+
+    // 글 정보 반환
+    res.json(posts); 
+});
+
+// Path Variable 을 이용한 글 세부정보 라우팅
+app.get('/api/post/:id', (req, res) => {
+
+    const postId = Number(req.params.id); 
+    const post = posts.filter(post => {
+        return post.id === postId;  
+    }); 
+
+    // 글 세부정보 반환
+    res.json(post); 
+}); 
 
 
 app.listen(app.get('port'), () => {
